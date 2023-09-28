3f479c41b8826868c399132a26e7878de8828cb2
Octavio <octiduarte@hotmail.com>
2023-09-28 19:38:56 -0300

    arreglo carro

--- carrouselImagenes/carrouselImagenes.js
+++ carrouselImagenes/carrouselImagenes.js
@@ -1,20 +1,20 @@
-const noticias = document.querySelectorAll(".noticia-item");
-let noticiaActual = 0;
+const imagenes = document.querySelectorAll(".imagen-item");
+let imagenActual = 0;
 
 const btnAnterior = document.getElementById("btnAnterior");
 const btnSiguiente = document.getElementById("btnSiguiente");
 
 btnAnterior.addEventListener("click", () => {
-    //ocultamos la noticia actual
-    noticias[noticiaActual].style.display = "none";
+    //ocultamos la imagen actual
+    imagenes[imagenActual].style.display = "none";
     //calculamos la nueva posicion
-    noticiaActual = (noticiaActual - 1 + noticias.length) % noticias.length;
-    //mostramos la nueva noticia
-    noticias[noticiaActual].style.display = "block";
+    imagenActual = (imagenActual - 1 + imagenes.length) % imagenes.length;
+    //mostramos la nueva imagen
+    imagenes[imagenActual].style.display = "block";
 });
 
 btnSiguiente.addEventListener("click", () => {
-    noticias[noticiaActual].style.display = "none";
-    noticiaActual = (noticiaActual + 1) % noticias.length;
-    noticias[noticiaActual].style.display = "block";
+    imagenes[imagenActual].style.display = "none";
+    imagenActual = (imagenActual + 1) % imagenes.length;
+    imagenes[imagenActual].style.display = "block";
 });

e4d079e8467978e672502feb017fb433247e4297
Octavio <octiduarte@hotmail.com>
2023-11-06 21:08:01 -0300

    iniciosesion

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ conectar();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Proyecto paradigmas</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet" />
@@ -24,8 +23,7 @@ conectar();
   <header class="header_estilo">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="index.php">Air.</a>
@@ -42,8 +40,20 @@ conectar();
             </li>
           </ul>
           <div>
-            <a class="nav_a" href="index.php?modulo=registro">Registrarse</a>
-            <a class="nav_a" href="">Iniciar Sesion</a>
+            <?php
+            if (!empty($_SESSION['nombre_usuario'])) {
+            ?>
+              <p>Bienvenido <?php echo $_SESSION['nombre_usuario']; ?></p>
+              <a href="index.php?modulo=iniciar_sesion&salir=ok" class="nav_a">Cerrar Sesión</a>
+            <?php
+            } else {
+            ?>
+              <a class="nav_a" href="index.php?modulo=registro">Registrarse</a>
+              <a class="nav_a" href="index.php?modulo=iniciar_sesion">Iniciar Sesion</a>
+            <?php
+            }
+            ?>
+
           </div>
         </div>
       </div>
@@ -54,7 +64,7 @@ conectar();
     if (!empty($_GET['modulo'])) {
       include('modulos/' . $_GET['modulo'] . '.php');
     } else {
-      ?>
+    ?>
       <div id="popup" style="display: none">
         <div class="content-popup">
           <div>
@@ -73,8 +83,7 @@ conectar();
                 </p>
                 <form action="">
                   <div class="col-12">
-                    <input type="text" class="form-control mail_popup" id="inputAddress"
-                      placeholder="Correo electronico" />
+                    <input type="text" class="form-control mail_popup" id="inputAddress" placeholder="Correo electronico" />
                   </div>
                   <div class="col-12">
                     <button type="submit" class="btn btn-light confirmar_popup">
@@ -112,25 +121,21 @@ conectar();
             <img src="imagenes/CarrouselInicio/remera3.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
-          data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
-          data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-      <?php
+    <?php
     }
     ?>
   </main>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </body>
 
---
+++ modulos/iniciar_sesion.php
@@ -0,0 +1,39 @@
+<?php
+if (isset($_GET['salir'])) {
+    session_destroy();
+    echo "<script>window.location='index.php';</script>";
+}
+if (isset($_POST['correo']) && isset($_POST['clave'])) {
+    $sql = "SELECT * FROM usuarios WHERE correo = '" .$_POST['correo']."' AND clave='".$_POST['clave']."'";
+    $sql = mysqli_query($con, $sql);
+    if (mysqli_num_rows($sql) != 0) {
+        $r = mysqli_fetch_array($sql);
+        $_SESSION['id'] = $r['id'];
+        $_SESSION['nombre_usuario'] = $r['correo'];
+        echo "<script> alert ('Bienvenido: " . $_SESSION['nombre_usuario'] . "');</script>";
+        //crear la sesion
+    } else {
+        echo "<script> alert('Verifique los datos.');</script>";
+    }
+    echo "<script>window.location='index.php';</script>";
+}
+?>
+
+
+<div>
+    <h1>Iniciar Sesion</h1>
+    <hr>
+</div>
+<div class="org_registro">
+    <form class="org_form_registro" action="index.php?modulo=iniciar_sesion" method="POST">
+        <div class="mb-3">
+            <label class="form-label">Correo electronico</label>
+            <input type="email" class="form-control" id="correo" name="correo">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="clave" name="clave">
+        </div>
+        <button type="submit" class="btn btn-dark">Loguearse</button>
+    </form>
+</div>

e663c497d8d7eddd15c9ec27680fed3980df1102
octiduarte <octiduarte@hotmail.com>
2023-11-07 10:22:17 -0300

    cambio de box

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ conectar();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Proyecto paradigmas</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet" />
@@ -23,7 +24,8 @@ conectar();
   <header class="header_estilo">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="index.php">Air.</a>
@@ -42,15 +44,17 @@ conectar();
           <div>
             <?php
             if (!empty($_SESSION['nombre_usuario'])) {
-            ?>
-              <p>Bienvenido <?php echo $_SESSION['nombre_usuario']; ?></p>
+              ?>
+              <p>Bienvenido
+                <?php echo $_SESSION['nombre_usuario']; ?>
+              </p>
               <a href="index.php?modulo=iniciar_sesion&salir=ok" class="nav_a">Cerrar Sesión</a>
-            <?php
+              <?php
             } else {
-            ?>
+              ?>
               <a class="nav_a" href="index.php?modulo=registro">Registrarse</a>
               <a class="nav_a" href="index.php?modulo=iniciar_sesion">Iniciar Sesion</a>
-            <?php
+              <?php
             }
             ?>
 
@@ -64,7 +68,7 @@ conectar();
     if (!empty($_GET['modulo'])) {
       include('modulos/' . $_GET['modulo'] . '.php');
     } else {
-    ?>
+      ?>
       <div id="popup" style="display: none">
         <div class="content-popup">
           <div>
@@ -73,8 +77,6 @@ conectar();
                 <div class="close">
                   <a href="#" id="close">X</a>
                 </div>
-                <img class="imagenGrandePopup" src="imagenes/popup/remera3.jpg" alt="" />
-                <img class="imagenChicaPopup" src="imagenes/popup/remera3Movil.jpg" alt="" />
               </div>
               <div class="parte_derecha_popup">
                 <h1>15% por tu primera compra</h1>
@@ -82,8 +84,9 @@ conectar();
                   Completa tu correo para recibir el regalo!
                 </p>
                 <form action="">
-                  <div class="col-12">
-                    <input type="text" class="form-control mail_popup" id="inputAddress" placeholder="Correo electronico" />
+                  <div class="col-12 centrado_popup">
+                    <input type="text" class="form-control mail_popup" id="inputAddress"
+                      placeholder="Correo electronico" />
                   </div>
                   <div class="col-12">
                     <button type="submit" class="btn btn-light confirmar_popup">
@@ -109,7 +112,7 @@ conectar();
           <a class="main_div_a main_div_a_yellow main_div_a-hover" href="index.php?modulo=box">Box</a>
         </div>
       </div>
-      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div id="carouselExampleInterval" class="carousel slide carrousel_org" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="2000">
             <img src="imagenes/CarrouselInicio/Remera.jpg" class="d-block w-100" alt="...">
@@ -121,22 +124,44 @@ conectar();
             <img src="imagenes/CarrouselInicio/remera3.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-    <?php
+      <?php
     }
     ?>
   </main>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+  <script>
+    $(document).ready(function () {
+      // Establecer un tiempo de espera de 3 segundos (3000 milisegundos)
+      setTimeout(function () {
+        // Abrir el popup
+        $("#popup").fadeIn("slow");
+        $(".popup-overlay").fadeIn("slow");
+        $(".popup-overlay").height($(window).height());
+      }, 1000); // Fin del tiempo de espera
+
+      // Cerrar el popup al hacer clic en el botón de cerrar
+      $("#close").on("click", function () {
+        $("#popup").fadeOut("slow");
+        $(".popup-overlay").fadeOut("slow");
+        return false;
+      });
+    });
+  </script>
 </body>
 
 </html>
--- modulos/box.php
+++ modulos/box.php
@@ -1,54 +1,47 @@
-<html>
-<div class="main_organizacion_box">
-    <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
+<div class="org_boxes">
+    <div class="org_box">
+        <img src="imagenes/Box/Remera1.jfif" alt="">
+        <div>
+            <h2>Remera Running Fila Performance Ii Hombre</h2>
+            <hr>
+            <p>Entrená con la comodidad que te merecés con la Remera Running Fila Performance Ii Hombre. Fabricada con
+                materiales de alta calidad, brinda máximo confort y elasticidad para que te muevas libremente y sin
+                restricciones. Mantené tu comodidad durante todo el día con la tecnología UV PROTECTION que te protege
+                del
+                sol y de los rayos UVA y UVB, mientras que el sistema FLOW proporciona un secado rápido para que
+                mantengas
+                tu piel fresca y seca durante todo el ejercicio. Como si fuera poco, la tecnología BREEZE está compuesta
+                por
+                un hilo especial capaz de reducir la temperatura corporal en 1,5ºC para que ni los calores intensos te
+                detengan.</p>
+            <h3>13000$</h3>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="imagenes/Movil/Remera1.jpg" class="d-block w-100 producto_imagen_chica1" alt="" />
-                <img src="imagenes/Remera.jpg" class="d-block w-100 producto_imagen_grande1" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Harcore pleasure black</h5>
-                    <p>15000$</p>
-                    <a class="btn btn-light" href="index.php?modulo=producto">Ver</a>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="imagenes/Movil/remera2.jpeg" class="d-block w-100 producto_imagen_chica2" alt="" />
-                <img src="imagenes/remera2.jpeg" class="d-block w-100 producto_imagen_grande2" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>iron Maiden black</h5>
-                    <p>17000$</p>
-                    <a class="btn btn-light" href="index.php?modulo=producto">Ver</a>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="imagenes/Movil/remera3.jpg" class="d-block w-100 producto_imagen_chica3" alt="" />
-                <img src="imagenes/remera3.jpg" class="d-block w-100 producto_imagen_grande3" alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Candy</h5>
-                    <p>20000$</p>
-                    <a class="btn btn-light" href="index.php?modulo=producto">Ver</a>
-                </div>
-            </div>
+    </div>
+    <div class="org_box">
+        <img src="imagenes/Box/Remera2.jfif" alt="">
+        <div>
+            <h2>Remera Puma Power Colorblock Hombre</h2>
+            <hr>
+            <p>Sentite siempre en movimiento con la Remera Puma Power Colorblock Hombre. Una prenda liviana, cómoda y
+                fresca
+                que te permite comenzar y terminar la rutina sin incomodidad. Con un diseño moderno y con el logo de la
+                marca en el frente te asegura la calidad y seguridad que buscas en una prenda. Combinala con tus prendas
+                deportivas o urbanas favoritas y asegúrate un look de otro nivel.</p>
+            <h3>12000$</h3>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+    </div>
+    <div class="org_box">
+        <img src="imagenes/Box/Remera3.jfif" alt="">
+        <div>
+            <h2>Remera Puma Ess Logo Hombre</h2>
+            <hr>
+            <p>
+                Rebosante de comodidad y estilo, la Remera Puma Ess Logo Hombre es tu infaltable de todos los días.
+                Confeccionada con el algodón mas suave y con un diseño clásico y versátil que se adapta a todos tus
+                planes.
+                Esta prenda te acompaña sin importar a donde vayas, desde una juntada con amigos hasta un día de
+                entrenamiento. Usala y asegúrate un estilo increíble de la mano de Puma.</p>
+            <h3>15000$</h3>
+        </div>
     </div>
 </div>
-</html>
-

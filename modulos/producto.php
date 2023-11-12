83971aef07a5048c6392862ee84277ab45d74bdb
Octavio Duarte <octiduarte@hotmail.com>
2023-11-12 10:14:46 -0300

    casi termin ado

--- modulos/producto.php
+++ modulos/producto.php
@@ -29,7 +29,17 @@
             <strong>Stock disponible</strong>
             <a href="">Devolucion gratis</a>
             <p>Tenes 30 dias desde que lo recibis</p>
-            <a class="nav_a" href="">Agregar a carrito</a>
+            <?php
+            if (!empty($_SESSION['nombre_usuario'])) {
+                ?>
+            <form action="index.php?modulo=box&accion=agregar_carrito&id=<?php echo $r['id']; ?>" method="post">
+                <label for="cantidad">Cantidad:</label>
+                <input type="number" id="cantidad" name="cantidad" value="1" min="1">
+                <input class="nav_a" type="submit" value="Agregar al carrito">
+            </form>
+            <?php
+            }
+            ?>
         </div>
     </div>
     <?php
